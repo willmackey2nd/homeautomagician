4367fdeb6c1a5c28392ed91fcd0acc47560001c9
willmackey2nd <67840987+willmackey2nd@users.noreply.github.com>
2022-12-14 21:55:03 +0200

    cleaned and working base

--- Env sensors/mpl/mpl.ino
+++ Env sensors/mpl/mpl.ino
@@ -1,13 +1,9 @@
 #include <SPI.h>
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
-#include "DHT.h" // Modified lines 155, 156 and 160 !!!
 #include "wificonfig.h"
 #include <OneWire.h>
 #include <DallasTemperature.h>
-
-/////// NODE SPECIFIC!!!!! ///////
-#define GATEWAYNR 1 // Nr of this gateway
 
 // Debug prints
 #define DEBUG
@@ -22,19 +18,12 @@
 
 
 // Pins
-#define DHTPIN D1     // what digital pin the DHT22 is conected to
-#define DHTPWRPIN D0     // what digital pin the DHT22 is conected to
-#define DHTTYPE DHT22   // there are multiple kinds of DHT sensors
+#define ONE_WIRE_BUS_PIN D2
 
-#define NF24_CE_PIN D2
-#define NF24_CSN_PIN D8
 
-#define INTSENSUPDINTERVAL 600000 // Internal sensor update interval (ms)
-RF24 radio(D2, D8); // CE, CSN
-uint8_t addresses[][6] = {"1GW01", "2GW01", "3GW01", "4GW01", "5GW01", "6GW01",}; // syntax xGWyy x = sensor number yy = gateway (nf24 only supports 6 clients)
-char address[6];
+#define INTSENSUPDINTERVAL 15000 // Internal sensor update interval (ms)
 
-char* NodeName = "sensorgwyy";
+char* NodeName = "mlp";
 const char* ssid = WIFI_SSID;
 const char* password =  WIFI_PASS;
 const char* mqttServer = MQTT_SERVER_IP;
@@ -42,7 +31,12 @@ const int mqttPort = MQTT_SERVER_PORT;
 
 WiFiClient espClient;
 PubSubClient client(espClient);
-DHT dht(DHTPIN, DHTTYPE);
+
+// Setup a oneWire instance to communicate with any OneWire device
+OneWire oneWire(ONE_WIRE_BUS_PIN);
+
+// Pass oneWire reference to DallasTemperature library
+DallasTemperature tempSensors(&oneWire);
 
 struct sensorpacket {
   char id[8] = "self"; //"SGW0001";
@@ -64,6 +58,7 @@ struct sensorpacket {
 char tmpstring[50];
 char tmpstring2[50];
 long prevUpdMillis = -INTSENSUPDINTERVAL;
+uint8_t deviceCount;
 
 void setup() {
 
@@ -72,35 +67,24 @@ void setup() {
 #endif
   delay(500);
 
-
-  // Node name
-  sprintf(NodeName, "sensorgw%d", GATEWAYNR); // 'sensorgwYY
-  
-  //nRF24 addresses to listen
-  for (int i = 1; i <= sizeof(addresses) / sizeof(addresses[0]); i++) {
-    sprintf(address, "%dGW0%d", i, GATEWAYNR); // syntax xGWyy x = sensor number yy = gateway
-    memcpy(addresses[i], address, sizeof(addresses[i]));
-    PRINT("nRF24 sensor address ");
-    for (int j = 0; j < sizeof(addresses[i]); j++) {
-      PRINT((char)addresses[i][j]);
-    }
-    PRINTL("");
-  }
-
-
   // IO
   pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(DHTPWRPIN, OUTPUT);
-  pinMode(DHTPIN, INPUT );
-
   digitalWrite(LED_BUILTIN, HIGH);
 
+  // OneWire
+  tempSensors.begin();
+  // locate devices on the bus
+  PRINT("Locating devices...");
+  PRINT("Found ");
+  deviceCount = tempSensors.getDeviceCount();
+  PRINT(deviceCount);
+  PRINTL(" devices.");
+  PRINTL("");
 
   delay(500);
   PRINT("configured 433 packet size: "); PRINTL(sizeof(sensorpacket));
 
   // WiFi
-
 
   // We start by connecting to a WiFi network
   PRINTL();
@@ -109,7 +93,6 @@ void setup() {
   PRINTL(ssid);
 
   WiFi.hostname(NodeName);
-
   WiFi.begin(ssid, password);
 
   int retrycnt = 0;
@@ -129,26 +112,8 @@ void setup() {
   PRINTL("IP address: ");
   PRINTL(WiFi.localIP());
 
-
-
   client.setServer(mqttServer, mqttPort);
   //client.setCallback(callback);
-
-  radio.begin();
-
-  radio.setPALevel(RF24_PA_MAX);
-  radio.setDataRate(RF24_250KBPS );
-  // Open all 6 pipes for listening
-  for (uint8_t i = 0; i <= 5; i++) {
-    radio.openReadingPipe(i, addresses[i]);
-  }
-  //  radio.openReadingPipe(0, addresses[0]);
-  //  radio.openReadingPipe(1, addresses[1]);
-  //  radio.openReadingPipe(2, addresses[2]);
-  //  radio.openReadingPipe(3, addresses[3]);
-  //  radio.openReadingPipe(4, addresses[4]);
-  //  radio.openReadingPipe(5, addresses[5]);
-  radio.startListening();
 
   PRINTL("init done");
   digitalWrite(LED_BUILTIN, LOW);
@@ -172,71 +137,46 @@ void loop() {
     internalSensors();
     prevUpdMillis = millis();
   }
-
-  uint8_t buf[sizeof(sensorpacket)];
-  uint8_t buflen = sizeof(buf);
-  byte pipeNum = 0; //variable to hold which reading pipe sent data
-  if (radio.available(&pipeNum)) {
-    /* We can ignore the pipe of received packet as the packet
-       contains sensor ID that's used to transfer data
-       under correct MQTT topic
-    */
-
-    //   PRINTL("received shit");
-    radio.read(&buf, sizeof(sensorpacket));
-
-    digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
-    // Message with a good checksum received, dump it.
-    //driver.printBuffer("Got:", buf, buflen);
-
-    struct sensorpacket sensorx;
-    memcpy(&sensorx, buf, sizeof(sensorpacket));
-    Mqttpub(NodeName, sensorx.id, sensorx.type, sensorx.payload);
-
-  }
 }
 
 
 void internalSensors() {
   struct sensorpacket intsens;
-  digitalWrite(DHTPWRPIN, HIGH);
-  dht.begin();
-  delay(2000); // Need to wait at least 2s for stable readings
-  String("self").toCharArray(intsens.id, 8);
+  char* sensorID = "tx";
 
+  
 
-  float rh = dht.readHumidity();
-  float temp = dht.readTemperature();
+  // Send command to all the sensors for temperature conversion
+  tempSensors.requestTemperatures();
 
-  // Temperature
-  if (isnan(rh)) {
-    PRINTL("rh error");
-    intsens.payload = -2000;
-  } else {
-    intsens.payload = (int)(temp * 10.0);
+  // Display temperature from each sensor
+  float temp;
+  for (uint8_t i = 0;  i < deviceCount;  i++)
+  {
+    sprintf(intsens.id, "t%d", i);
+    //strcpy(intsens.id, sensorID);
+    //String("self").toCharArray(intsens.id, 8);
+
+    // Temperature
+    temp = tempSensors.getTempCByIndex(i);
+    if (isnan(temp)) {
+      PRINTL("temp error");
+      intsens.payload = -2000;
+    } else {
+      intsens.payload = (int)(temp * 10.0);
+    }
+    Mqttpub(NodeName, intsens.id, 1, intsens.payload);
+
+    PRINT("Sensor ");
+    PRINT(i + 1);
+    PRINT(" : ");
+    PRINT(temp);
+    PRINT((char)176);//shows degrees character
+    PRINT("C  |  ");
+    PRINT(DallasTemperature::toFahrenheit(temp));
+    PRINT((char)176);//shows degrees character
+    PRINTL("F");
   }
-  Mqttpub(NodeName, intsens.id, 1, intsens.payload);
-
-
-  // Humidity
-  if (isnan(rh)) {
-    PRINTL("temp error");
-    intsens.payload = -2000;
-  } else {
-    intsens.payload = (int)(rh * 10.0);
-  }
-  Mqttpub(NodeName, intsens.id, 2, intsens.payload);
-
-  digitalWrite(DHTPWRPIN, LOW);
-  digitalWrite(DHTPIN, LOW);
-
-  // intsens.rh = (int)(dht.readHumidity() * 10);
-  // intsens.temp = (int)(dht.readTemperature() * 10);
-
-  //PRINT("Internal sensor "); PRINT((float)intsens.temp/10.0); PRINT("C "); PRINT((float)intsens.rh/10.0); PRINTL(" %rh");
-
-
-  // ths(intsens);
 }
 
 
@@ -337,70 +277,12 @@ void FormatPayload(char* payloadbuf, int8_t sensorType, int16_t rawPayload) {
 }
 
 
-//void ths(struct sensorpacket &sensor) {
-//
-//  /*
-//   -1000 = sensor not available
-//   -2000 = sensor error
-//   */
-//
-//
-//  //client.publish(tmpstring, String((float)sensor.temp/10.0).c_str());
-//  char valarr[10] = "";
-//
-//  // Power supply
-//  sprintf(tmpstring, "devices/%s/sensors/%s/supplyvoltage", NodeName, sensor.id);
-//  if (sensor.pwr > -1000) {
-//    sprintf(valarr, "%.2f", (float)sensor.pwr / 1000.0);
-//    client.publish(tmpstring, valarr);
-//  } else if (sensor.pwr == -2000) {
-//    client.publish(tmpstring, "error");
-//  }
-//
-//  // Temperature
-//  sprintf(tmpstring, "devices/%s/sensors/%s/temperature", NodeName, sensor.id);
-//  if (sensor.temp > -1000) {
-//    sprintf(valarr, "%.1f", (float)sensor.temp / 10.0);
-//    client.publish(tmpstring, valarr);
-//  } else if (sensor.temp == -2000) {
-//    client.publish(tmpstring, "error");
-//  }
-//
-//  // Humidity
-//  sprintf(tmpstring, "devices/%s/sensors/%s/humidity", NodeName, sensor.id);
-//  if (sensor.rh > -1000) {
-//    sprintf(valarr, "%.1f", (float)sensor.rh / 10.0);
-//    client.publish(tmpstring, valarr);
-//  } else if (sensor.rh == -2000) {
-//    client.publish(tmpstring, "error");
-//  }
-//
-//  // Pressure
-//  sprintf(tmpstring, "devices/%s/sensors/%s/pressure", NodeName, sensor.id);
-//  if (sensor.p > -1000) {
-//    sprintf(valarr, "%d", sensor.p);
-//    client.publish(tmpstring, valarr);
-//  } else if (sensor.p == -2000) {
-//    client.publish(tmpstring, "error");
-//  }
-//
-//  // Luminosity
-//  sprintf(tmpstring, "devices/%s/sensors/%s/luminosity", NodeName, sensor.id);
-//  if (sensor.c > -1000) {
-//    sprintf(valarr, "%d", sensor.c);
-//    client.publish(tmpstring, valarr);
-//  } else if (sensor.c == -2000) {
-//    client.publish(tmpstring, "error");
-//  }
-//}
-
-
 void reconnect() {
   // Loop until we're reconnected
   while (!client.connected()) {
     PRINT("Attempting MQTT connection...");
     // Attempt to connect
-    if (client.connect(NodeName)) {
+    if (client.connect(NodeName, MQTT_USR, MQTT_PW)) {
       PRINTL("connected");
       sprintf(tmpstring, "devices/%s/status", NodeName);
 
