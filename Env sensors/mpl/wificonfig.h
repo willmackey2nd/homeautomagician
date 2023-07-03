67d8766cdb2153b4b53725ad37657a1d1a600421
willmackey2nd <67840987+willmackey2nd@users.noreply.github.com>
2023-07-03 15:16:04 +0300

    aa

--- Env sensors/mpl/wificonfig.h
+++ Env sensors/mpl/wificonfig.h
@@ -1,6 +1,6 @@
 #define WIFI_PASS "hevi00Hemuli"
-#define WIFI_SSID "IoT24"
+#define WIFI_SSID "ChuckOfThings"
 #define MQTT_SERVER_IP "192.168.1.201"
 #define MQTT_SERVER_PORT 1883
-#define MQTT_USR "mqtt-user"
+#define MQTT_USR "mqtt"
 #define MQTT_PW "FJxvHPt37mLP2g"
